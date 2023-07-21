9528151ca213bb7d0f95d412b43545ad41a11d2b
kadenmusic <kaden.music.jr@gmail.com>
2023-07-21 10:45:44 -0700

    Initial commit.

---
+++ index.js
@@ -0,0 +1,45 @@
+const cool = require('cool-ascii-faces')
+const express = require('express')
+const path = require('path')
+const { Pool } = require('pg')
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+})
+
+const PORT = process.env.PORT || 8080;
+const HOST = '0.0.0.0';
+
+express()
+  .use(express.static(path.join(__dirname, 'public')))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
+  .get('/', (req, res) => res.render('pages/index'))
+  .get('/cool', (req, res) => res.send(cool()))
+  .get('/times', (req, res) => res.send(showTimes()))
+  .get('/db', async (req, res) => {
+    try {
+      const client = await pool.connect();
+      const result = await client.query('SELECT * FROM test_table');
+      const results = { 'results': (result) ? result.rows : null};
+      res.render('pages/db', results );
+      client.release();
+    } catch (err) {
+      console.error(err);
+      res.send("Error " + err);
+    }
+  })
+  .listen(PORT, HOST, () => console.log(`Listening on ${ PORT }`))
+
+
+function showTimes() {
+  const times = process.env.TIMES || 5
+  let result = ''
+  for (i = 0; i < times; i++) {
+    result += i + ' '
+  }
+  return result
+}
