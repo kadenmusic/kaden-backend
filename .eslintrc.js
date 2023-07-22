6214a533d36d156a33a786fd5dd6a0e4626fa232
kadenmusic <kaden.music.jr@gmail.com>
2023-07-22 15:17:43 -0700

    add prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,10 @@
 module.exports = {
-  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint'],
-  "ignorePatterns": ["**/dist/*", ".eslintrc.js"],
+  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint"],
+  ignorePatterns: ["**/dist/*", ".eslintrc.js"],
   root: true,
   rules: {
-    '@typescript-eslint/no-var-requires': 0
-  }
+    "@typescript-eslint/no-var-requires": 0,
+  },
 };
