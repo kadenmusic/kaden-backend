cfc65101dbc4c7a37d1b3f6a90ce92a42a1abc07
kadenmusic <kaden.music.jr@gmail.com>
2023-07-23 12:21:30 -0700

    add mocha

--- dist/index.js
+++ dist/index.js
@@ -9,31 +9,34 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
     });
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const cool = require('cool-ascii-faces');
-const express = require('express');
-const path = require('path');
-const { Pool } = require('pg');
+const cool = require("cool-ascii-faces");
+const express = require("express");
+const path = require("path");
+const { Pool } = require("pg");
 // const DATABASE_URL = 'postgres://username:password@localhost:5435/database-name?sslmode=disable';
 const pool = new Pool({
-    connectionString: process.env.DATABASE_URL || 'postgres://username:password@localhost:5435/database-name?sslmode=disable',
-    ssl: process.env.DATABASE_URL ? {
-        rejectUnauthorized: false
-    } : false
+    connectionString: process.env.DATABASE_URL ||
+        "postgres://username:password@localhost:5435/database-name?sslmode=disable",
+    ssl: process.env.DATABASE_URL
+        ? {
+            rejectUnauthorized: false,
+        }
+        : false,
 });
 const PORT = process.env.PORT || 8080;
 // const HOST = '0.0.0.0';
 express()
-    .use(express.static(path.join(__dirname, '../public')))
-    .set('views', path.join(__dirname, '../views'))
-    .set('view engine', 'ejs')
-    .get('/', (req, res) => res.render('pages/index'))
-    .get('/cool', (req, res) => res.send(cool()))
-    .get('/db', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    .use(express.static(path.join(__dirname, "../public")))
+    .set("views", path.join(__dirname, "../views"))
+    .set("view engine", "ejs")
+    .get("/", (req, res) => res.render("pages/index"))
+    .get("/cool", (req, res) => res.send(cool()))
+    .get("/db", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     try {
         const client = yield pool.connect();
-        const result = yield client.query('SELECT * FROM test_table');
-        const results = { 'results': (result) ? result.rows : null };
-        res.render('pages/db', results);
+        const result = yield client.query("SELECT * FROM test_table");
+        const results = { results: result ? result.rows : null };
+        res.render("pages/db", results);
         client.release();
     }
     catch (err) {
