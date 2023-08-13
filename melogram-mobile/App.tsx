a7c4213ba656251e759d5196770d167b0eee0a48
stevenpenavajr <steven.penava@gmail.com>
2023-08-12 22:07:17 -0700

    clean up font loading

--- melogram-mobile/App.tsx
+++ melogram-mobile/App.tsx
@@ -1,21 +1,34 @@
-import React, { useCallback } from "react";
+import React, { useCallback, useEffect } from "react";
 import { NativeBaseProvider, Center, Text, View, StatusBar } from "native-base";
 import { useFonts } from "expo-font";
 import * as SplashScreen from "expo-splash-screen";
 import { CUSTOM_FONTS } from "./src/config/constants";
+import * as Font from "expo-font";
 import { theme } from "./src/styles/theme";
 import AppNavigatorComponent from "./src/components/navigation/app-navigator.component";
 
 export default function App() {
-  const [fontsLoaded] = useFonts(CUSTOM_FONTS);
+  const [appReady, setAppReady] = React.useState(false);
 
-  const onLayoutRootView = useCallback(async () => {
-    if (fontsLoaded) {
+  useEffect(() => {
+    const loadFonts = async () => {
+      await Font.loadAsync(CUSTOM_FONTS);
+    };
+
+    loadFonts()
+      .then(() => {
+        setAppReady(true);
+      })
+      .catch((error) => {});
+  }, []);
+
+  useCallback(async () => {
+    if (appReady) {
       await SplashScreen.hideAsync();
     }
-  }, [fontsLoaded]);
+  }, [appReady]);
 
-  if (!fontsLoaded) {
+  if (!appReady) {
     return null;
   }
 
